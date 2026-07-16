ed40d5d0808767815d32e313760ecdb5d36e7a15
Felix <fschwieb@protonmail.com>
2026-07-16 19:19:53 -0400

    Include favicon in new window creation
    Add favicon link to the new window for better branding.

--- abc.js
+++ abc.js
@@ -3,6 +3,12 @@ function cloak() {
   w.document.body.style.margin = "0";
   
   let iframe = w.document.createElement('iframe');
+  let favicon = w.document.createElement('link');
+  
+  favicon.href = "https://cdn.jsdelivr.net/gh/ohio-math/cloaks/gdrive_new.png";
+  favicon.rel = "icon";
+  favicon.type = "image/png";
+  
   iframe.style.height = "100dvh";
   iframe.style.width = "100vw";
   iframe.style.border = "none";
@@ -10,6 +16,8 @@ function cloak() {
   iframe.src = window.location.href;
 
   w.document.body.appendChild(iframe);
+  w.document.head.appendChild(favicon);
+  w.title = "Home - Google Drive";
 }
 
 window.onload = () => {
